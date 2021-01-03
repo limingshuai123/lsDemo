69f37ffe863831d03893cf996b14a8244f6a7d98
ms <2290961414@qq.com>
2021-01-03 20:46:47 +0800

    '提交';

---
+++ src/com/entity/Hello.java
@@ -0,0 +1,12 @@
+package com.entity;
+
+/**
+ * @Auther 笙
+ * @Date 2021/1/3
+ **/
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("你好呀!");
+    }
+}
